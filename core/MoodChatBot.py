ce68a752188aef4fbd0d06088e58331cbf680fcb
atty22 <attilio2.baratta@gmail.com>
2021-04-13 02:06:02 +0200

    if only one replay use it

--- core/MoodChatBot.py
+++ core/MoodChatBot.py
@@ -6,12 +6,13 @@ from utils import mood, get_messages, mood_to_words
 
 @sanya.on(events.NewMessage)
 async def mood_chat_bot_handler(event):
+    _mood = mood()
     if event.is_private or event.is_group and config["!DEFAULT!"]["bot_name"] in event.raw_text.lower():
         available_keys = ({**get_messages("normal", "mood_chat_bot"), **get_messages("groups_auto", "mood_chat_bot")}
                           if event.message.reply_to_msg_id is None
                           else {**get_messages("normal", "mood_chat_bot"), **get_messages("reply"),
                                 **get_messages("groups_auto", "mood_chat_bot")})
-        time_key = get_messages("exceptions", "mood_chat_bot")["time"][mood_to_words(mood())]
+        time_key = get_messages("exceptions", "mood_chat_bot")["time"][mood_to_words(_mood)]
         for word in time_key:
             if word in event.raw_text.lower():
                 await event.reply(time_key[word][randrange(0, len(time_key[word]))])
@@ -20,8 +21,7 @@ async def mood_chat_bot_handler(event):
             if word in event.raw_text.lower():
                 if len(available_keys[word]) !=3:
                     _mood=0
-                (await (event.reply(available_keys[word][( 0 if len(available_keys[word]) != 3 else mood())]))
-                 if available_keys[word][( 0 if len(available_keys[word]) !=3 else mood())] != "" else None)
+                (await (event.reply(available_keys[word][_mood])) if available_keys[word][_mood] != "" else None)
                 return
         if config["!DEFAULT!"]["bot_name"] in event.raw_text.lower().replace(" ", ""):
             await event.reply(get_messages("exceptions", "mood_chat_bot")["onlysanya"]
@@ -33,4 +33,6 @@ async def mood_chat_bot_handler(event):
                           {**get_messages("reply", "mood_chat_bot"), **get_messages("groups_auto", "mood_chat_bot")})
         for word in available_keys:
             if word in event.raw_text.lower():
-                await event.reply(available_keys[word][( 0 if len(available_keys[word]) !=3 else mood())])
+                if len(available_keys[word]) !=3:
+                    _mood=0
+                await event.reply(available_keys[word][_mood])
