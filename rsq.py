6d524bb60c90d6d5532fbc5b69a12792cc3d28e8
atty22 <attilio2.baratta@gmail.com>
2021-04-23 14:51:15 +0200

    added stuff

--- rsq.py
+++ rsq.py
@@ -6,23 +6,36 @@ from random import randrange
 from utils import get_messages
 from time import strftime
 import telebot  # pip install pyTelegramBotAPI
+from os import listdir
 
 bot_token = "1794569431:AAEnC99GBFyWJZvkL78G_oNeUyAsPhsW67Q"
 tb = telebot.TeleBot(bot_token)
 
 
 def birtday():
-    sql = sqlite3.connect('database.db')
-    cur = sql.cursor()
-    cur.execute("SELECT * FROM chat WHERE active= 1")
-    list = cur.fetchall()
-    sql.close()
-    id_list = [int(item[0]) for item in list]
-    print(id_list)
     messages = get_messages("schedule_messages")
     for item in messages["date"]:
         if item in strftime("%m %d"):
             for i in id_list:
                 tb.send_message(i, messages["date"][item])
+            return True
+        return False
 
-birtday()
+
+sql = sqlite3.connect('database.db')
+cur = sql.cursor()
+cur.execute("SELECT * FROM chat WHERE active= 1")
+list = cur.fetchall()
+sql.close()
+id_list = [int(item[0]) for item in list]
+
+if birtday() is False:
+    if randrange(0,5) == 3:
+        print("si")
+        audio = open(
+            str("messages/audio/" + listdir("messages/audio")[randrange(0, len(listdir("messages/audio")))]),
+            'rb')
+        for i in id_list:
+            tb.send_audio(chat_id, audio)
+    else:
+        print("no")
